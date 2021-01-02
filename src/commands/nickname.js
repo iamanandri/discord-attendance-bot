e9bbea4ac3abcf0acefce76e47892571fabe5bba
iamanandri <irfananandri@gmail.com>
2021-01-02 19:36:02 +0700

    commit

--- src/commands/nickname.js
+++ src/commands/nickname.js
@@ -1,18 +1,14 @@
-const { Message } = require("discord.js");
-
-/**
- * @typedef {{channelId: number, commandPrefix: string}} Args
- */
+const { Message, GuildMember } = require("discord.js");
 
 module.exports = {
     name: "nickname",
     description: "Changes the user's nickname in the server.",
     /**
      * @param {Message} message
-     * @param {Args} args
+     * @param {string} args
      */
     async execute(message, args) {
         /** @type {string?} */
-        let sand;
+        message.member.setNickname(args).catch(e => console.log(e));
     }
 }
